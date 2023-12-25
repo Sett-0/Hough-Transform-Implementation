08867e07e25857708d021645345bfed00f6335df
Sett-0 <ampilov2000@mail.ru>
2023-12-25 03:22:55 +0300

    a bit better

--- hough.py
+++ hough.py
@@ -1,10 +1,14 @@
 import cv2 as cv 
 import numpy as np 
 
-img = cv.imread("triangle.jpg")
-grayscale = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+filename = "house.jpg"
+img = cv.imread(filename)
+canny = cv.Canny(img, 100, 270)
 
-def Hough_transform(image, theta=np.pi/180, treshold=200):
+img1 = img.copy()
+img2 = img.copy()
+
+def Hough_transform(image, theta=np.pi/180, treshold=120):
     width, height = image.shape[1], image.shape[0]
     # rho = [0, (width + height) // 2], theta = [0, 2pi]
     num_thetas = round(2*np.pi / theta + 1)
@@ -30,11 +34,12 @@ def Hough_transform(image, theta=np.pi/180, treshold=200):
     lines = np.array(lines)
     if lines.size == 0: lines = None
     return lines
-lines = Hough_transform(grayscale, theta=np.pi/180, treshold=220)
-print(lines)
+
+# My Hough transform
+lines = Hough_transform(canny, theta=np.pi/180, treshold=170)
 if (lines is not None):
     for i, (rho, theta) in enumerate(lines):
-        width, height = grayscale.shape[1], grayscale.shape[0]
+        width, height = canny.shape[1], canny.shape[0]
         a = np.cos(theta)
         b = np.sin(theta)
         
@@ -46,34 +51,31 @@ if (lines is not None):
         x2 = int(x0 - 1000 * (-b)) + width // 2
         y2 = height // 2 - int(y0 - 1000 * a)
         
-        cv.line(img, (x1, y1), (x2, y2), (0,255,0), thickness=1)
+        cv.line(img1, (x1, y1), (x2, y2), (0,255,0), thickness=1)
         
-    cv.imshow("Triangle", img)
+    cv.imshow(filename, img1)
     cv.waitKey(0)
-# cv.destroyAllWindows()
+    cv.destroyAllWindows()
 
 
-
-# lines = cv.HoughLines(grayscale, rho=1, theta=np.pi/180, threshold=220)
-# if (lines is not None):
-#     for i in range(len(lines)):
-#         for rho, theta in lines[i]:
-#             a = np.cos(theta)
-#             b = np.sin(theta)
+# Actual Hough transform
+lines = cv.HoughLines(canny, rho=1, theta=np.pi/180, threshold=120)
+if (lines is not None):
+    for i in range(len(lines)):
+        for rho, theta in lines[i]:
+            a = np.cos(theta)
+            b = np.sin(theta)
             
-#             x0 = a * rho 
-#             y0 = b * rho 
+            x0 = a * rho 
+            y0 = b * rho 
             
-#             x1 = int(x0 + 1000 * (-b))
-#             y1 = int(y0 + 1000 * a)
-#             x2 = int(x0 - 1000 * (-b))
-#             y2 = int(y0 - 1000 * a)
+            x1 = int(x0 + 1000 * (-b))
+            y1 = int(y0 + 1000 * a)
+            x2 = int(x0 - 1000 * (-b))
+            y2 = int(y0 - 1000 * a)
             
-#             cv.line(img, (x1, y1), (x2, y2), (0,255,0), thickness=1)
+            cv.line(img2, (x1, y1), (x2, y2), (0,0,255), thickness=1)
         
-# cv.imshow("Triangle", img)
-# cv.waitKey(0)
-# # cv.destroyAllWindows()
+cv.imshow(filename, img2)
+cv.waitKey(0)
         
-
-
